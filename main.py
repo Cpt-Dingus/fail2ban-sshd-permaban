f4eb5eda89c065758ccbf326a93ea46b119c5106
Cpt-Dingus <meti17207@gmail.com>
2022-12-10 01:06:59 +0100

    v1.3 - Added depth setting, more advanced scanning of auth.log

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 # Made by Cpt-Dingus
-# v1.2.1 - 30.11.2022
+# v1.3 - 10.12.2022
 
 
 # -- Config --
 
 # Amount of bans before a permaban [Default = 3]
 ban_trigger = 3
+
+# Scanning depth, options:
+#  > [0] -> Only scan for fail2ban banned IPs [DEFAULT]
+#  > [1] -> Scan auth.log for IPs that sshd is unable to negotiate with (No matching MACs, later referred to as UNW)
+#  > [2] -> Scan auth.log for IPSs that are attempting to access root
+#  > [3] -> Scan auth.log for all invalid users
+#  > [4] -> All of the above
+# Note: You can select multiple options at once, eg: [13]
+depth = "0"
+
 
 
 # -- Imports --
@@ -14,10 +24,13 @@ import os
 import json
 import datetime
 
+
+
 # -- Vars --
 
 work_dir = os.path.dirname(os.path.realpath(__file__))  
 ip_dict = {}
+prev_dtc = False
 
 
 
@@ -28,36 +41,76 @@ print(f'PY: Script started at {datetime.datetime.now().strftime("%m/%d/%Y, %H:%M
 
 # Pulls IPs via a bash script, as python cannot directly run sudo commands
 print("PY: Calling load")
-os.system(f'sudo {os.path.join(work_dir, "main.sh")} -m load')
+os.system(f'sudo {os.path.join(work_dir, "main.sh")} -m load -d {depth}')
 
 
-# Loading JSON
+# Loading JSON lists
 ips_json = json.load(open("/tmp/ips.json", "r"))
-ip_list = ips_json['Fail2ban-ips'].split()
-banned_ips = ips_json['Permabanned-ips'].split()
+banned_ip_list = ips_json['Fail2ban-ips'].split() # Pre-parsed
+permabanned_ips = ips_json['Permabanned-ips'].split()
+UNW_list = ips_json['UNW'].split()
+root_list = ips_json['Root_list'].split()
+invalid_user_list = ips_json['Invalid_user_list'].split()
 
 
+# -- Defs --
+
+def parse_list(usr_list, trigger_word):
+    prev_dtc = True
+    buffer = []
+    global ip_dict
+    
+    # Parsing
+    for word in usr_list:
+        if prev_dtc is True:
+            buffer.append(word)
+            prev_dtc = False
+        
+        if word == trigger_word:
+            prev_dtc = True
+
+    # Appending to dict
+    for ip in buffer:
+        if not ip in ip_dict:
+            ip_dict[f'{ip}'] = 1
+        else:
+            ip_dict[f'{ip}'] += 1
+        
 
 # -- Main --
 
-# Add IPs to dictionary
-for ip in ip_list:
-    if not ip in ip_dict:
-        ip_dict[f'{ip}'] = 1
+# TODO: Make Banned list parsed in PY instead of SH
+# 0 - Banned check
+if "0" in depth or depth == "4": 
+    for ip in banned_ip_list:
+        if not ip in ip_dict:
+            ip_dict[f'{ip}'] = 1
+        else:
+            ip_dict[f'{ip}'] += 1
 
-    else:
-        ip_dict[f'{ip}'] += 1
+
+# 1 - UNW check
+if "1" in depth or depth == "4": parse_list(UNW_list, "with")
+
+# 2 - Root access check
+if "2" in depth or depth == "4":  parse_list(root_list, "root")
+
+# 3 - Invalid user check
+if "3" in depth or depth == "4":  parse_list(invalid_user_list, "from")
 
 
-# Add IP to hosts.deny if the file isn't there already
+
+
+# Add IP to hosts.deny if it isn't there already
 for ip in ip_dict:
-
-    if ip_dict[f'{ip}'] > ban_trigger and ip not in banned_ips:
+    if ip_dict[f'{ip}'] > ban_trigger and ip not in permabanned_ips:
         print(f"PY: Calling append of {ip}")
         os.system(f'sudo {os.path.join(work_dir, "main.sh")} -m append -i {ip}')
+
 
 
 # -- Cleanup --
 
 print("PY: Calling cleanup")
 os.system(f'sudo {os.path.join(work_dir, "main.sh")} -m cleanup')
+
--- main.sh
+++ main.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
 # Made by Cpt-Dingus
-# v1.2.2 - 04.12.2022
+# v1.3 - 10.12.2022
+
 
 
 # -- CLI args --
-while getopts i:m: flag
+
+while getopts m:i:d: flag
 do
     case "${flag}" in
-        i) ip=${OPTARG};;
 	m) mode=${OPTARG};;
+	i) ip=${OPTARG};;
+	d) depth=${OPTARG};;
     esac
 done
 
+# Mode - Options:
+#	  > load    -> Load selected files, send contents to ips.json in /tmp
+#	  > append  -> Append Ip parameter to /etc/hosts.deny
+# 	  > cleanup -> Remove ips.json from /tmp
+
+# Ip - Passed ip to append if Mode is [append]
+
+# Depth - Options:
+#         > [0] -> Only scan for fail2ban banned IPs [DEFAULT]
+#         > [1] -> Scan auth.log for IPs that sshd is unable to negotiate with (No matching MACs)
+#         > [2] -> Scan auth.log for IPSs that are attempting to access root 
+#         > [3] -> Scan auth.log for all invalid users
+# 	  > [4] -> All of the above
+# Note: You can select multiple options at once, eg: [13]
+
+
+
+# -- Processing 'mode' flag --
 
 # Cleans up temp file
 if [ "$mode" = "cleanup" ]; then
@@ -35,33 +56,74 @@ if [ "$mode" != "load" ]; then
 fi
 
 
+# TODO: If block borked
+#if [ "$(grep -vo 01234 <<< "$depth")" ]; then
+#	echo "SH: Wrong/no depth flag detected! Refer to the config, fix the -d flag accordingly."
+#	exit 1
+#fi
+
+
+
+
 # -- Vars --
 
-# Get list of banned IPs
-raw_list=$(sudo zgrep 'Ban' /var/log/fail2ban.log)
 
 # Get already permabanned IPs
-raw_banned_list=$(sudo zgrep 'sshd' /etc/hosts.deny)
+raw_permabanned_list=$(sudo zgrep 'sshd' /etc/hosts.deny)
 
-# Raw list of unable to negotiate with IPs (has to be parsed with PY)
-raw_UNW_list=$(sudo zgrep 'unable to negotiate with' /var/log/auth.log | tr : _ | tr -d '\n')
-
-
-prev_ban="False"
 prev_sshd="False"
-
-# Arrays of IPs
+prev_ban="False"
 ip_list=()
-banned_list=()
+permabanned_list=()
 
 
-# -- Main --
+
+# -- Optional vars --
+
+raw_ban_list="Skipping"
+
+if [[ $depth =~ 0 || $depth = "4" ]]; then
+	# Raw list of IPs banned by fail2ban (Will get parsed by SH)
+	raw_ban_list=$(sudo zgrep 'Ban' /var/log/fail2ban.log)
+fi
+
+
+# All of these are outputted raw, they will get parsed by the python script (PY).
+
+raw_UNW_list="Skipping"
+
+if [[ $depth =~ 1 || $depth = "4" ]]; then 
+	# Raw list of unable to negotiate with IPs
+	raw_UNW_list=$(sudo zgrep 'Unable to negotiate with' /var/log/auth.log | sudo zgrep 'sshd' | tr : _ | tr -d '\n')
+fi
+
+
+raw_root_list="Skipping"
+
+if [[ $depth =~ 2 || $depth = "4" ]]; then
+	# Raw list of IPs that tried to auth as root
+	raw_root_list=$(sudo zgrep 'Connection closed by authenticating user root' /var/log/auth.log | sudo zgrep 'sshd' | tr : _ | tr -d '\n')
+fi
+
+
+
+raw_invalid_user_list="Skipping"
+
+if [[ $depth =~ 3 || $depth = "4" ]]; then
+	# Raw list of IPs that tried to auth as an invalid user
+	raw_invalid_user_list=$(sudo zgrep 'Failed password for invalid user' /var/log/auth.log | sudo zgrep 'sshd' | sed "s/invalid user user/invalid user/" | tr : _ | tr -d '\n')
+
+fi
+
+
+
+# -- Parsing list --
 
 # Separates fail2ban log by spaces, if an IP is banned pulls it
-for item in $raw_list
+for item in $raw_ban_list
 do
 	if [ "$prev_ban" = "True" ]; then
-		ip_list+="${item/e/'\n'/} "
+		ip_list+=( "${item/e/'\n'/} " )
 		prev_ban="False"
 	fi
 
@@ -73,10 +135,10 @@ done
 
 
 # Separates hosts.deny, if an sshd IP is banned, pulls it
-for item in $raw_banned_list
+for item in $raw_permabanned_list
 do
 	if [ "$prev_sshd" = "True" ]; then
-		banned_list+="${item} "
+		permabanned_list+=( "${item} " )
 		prev_sshd="False"
 	fi
 
@@ -86,10 +148,9 @@ do
 done
 
 
+
 # -- Writing to temporary JSON --
 
-#rm -rf /tmp/ips.json  # Removes previous instance
-echo "SH: Cleaned up old ips.json"
-
-echo "{\"Fail2ban-ips\":\"$ip_list\",\"Permabanned-ips\":\"$banned_list\",\"UNW\":\"$raw_UNW_list\"}" > /tmp/ips.json
+echo "{\"Fail2ban-ips\":\"${ip_list[*]}\",\"Permabanned-ips\":\"${permabanned_list[*]}\",\"UNW\":\"$raw_UNW_list\",\"Root_list\":\"$raw_root_list\",\"Invalid_user_list\":\"$raw_invalid_user_list\"}" > /tmp/ips.json
 echo "SH: Made new ips.json in /tmp"
+
